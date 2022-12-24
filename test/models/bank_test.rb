156edb804b06012977e3b3a36339d2d60924c094
David <davidmesa02@gmail.com>
2022-12-23 21:03:41 -0500

    banks minitests

--- test/models/bank_test.rb
+++ test/models/bank_test.rb
@@ -4,4 +4,39 @@ class BankTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save bank without name" do
+    bank = Bank.new
+    assert_not bank.save, "Saved the bank without a name"
+  end
+
+  test "should save bank with name" do
+    bank = Bank.new
+    bank.name = "Bank"
+    assert bank.save, "Saved the bank without a name"
+  end
+
+  test "should not save with duplicate name" do
+    bank = Bank.new
+    bank.name = "Bank"
+    assert bank.save, "Saved the bank without a name"
+    bank = Bank.new
+    bank.name = "Bank"
+    assert_not bank.save, "Saved the bank without a name"
+  end
+
+  test "should not update bank without name" do
+    bank = Bank.new
+    bank.name = "Bank"
+    assert bank.save, "Saved the bank without a name"
+    bank.name = nil
+    assert_not bank.save, "Saved the bank without a name"
+  end
+
+  test "should update bank with name" do
+    bank = Bank.new
+    bank.name = "Bank"
+    assert bank.save, "Saved the bank without a name"
+    bank.name = "Bank2"
+    assert bank.save, "Saved the bank without a name"
+  end
 end
